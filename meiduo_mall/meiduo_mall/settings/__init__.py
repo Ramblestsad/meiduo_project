195e82b52ccfebfdd77039eabc33def10a6cbffa
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2020-12-21 19:00:37 +0800

    modify jinja2 # language

--- meiduo_mall/meiduo_mall/settings/__init__.py
+++ meiduo_mall/meiduo_mall/settings/__init__.py
@@ -2,15 +2,15 @@
 # -*- encoding: utf-8 -*-
 
 
-'''
+"""
 Author: Chris W.
 Date: 2020-12-21 18:06:09
-LastEditTime: 2020-12-21 18:06:10
+LastEditTime: 2020-12-21 19:00:09
 LastEditors: Chris W.
 Description: None
 Version: 1.0
 License: MIT License
 Copyright 2020 Chris W.
-'''
+"""
 
 
--- meiduo_mall/meiduo_mall/utils/jinja2_env.py
+++ meiduo_mall/meiduo_mall/utils/jinja2_env.py
@@ -2,16 +2,16 @@
 # -*- encoding: utf-8 -*-
 
 
-'''
+"""
 Author: Chris W.
 Date: 2020-12-21 18:42:39
-LastEditTime: 2020-12-21 18:49:51
+LastEditTime: 2020-12-21 18:59:55
 LastEditors: Chris W.
 Description: None
 Version: 1.0
 License: MIT License
 Copyright 2020 Chris W.
-'''
+"""
 
 
 from jinja2 import Environment
@@ -21,14 +21,14 @@ from django.urls import reverse
 
 def jinja2_environment(**options):
 
-    # 创建环境对象
+    # create env object
     env = Environment(**options)
 
-    # 自定义语法：{{ url('') }} {{ static('') }}
+    # custom grammar: {{ url('') }} {{ static('') }}
     env.globals.update({
         'static':staticfiles_storage.url,
         'url':reverse,
     })
 
-    # 返回环境对象
+    # return env object
     return env
