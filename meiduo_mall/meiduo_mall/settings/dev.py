500cd4bf904f4fd7df6d28b711523f9f798f03a9
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2020-12-21 20:23:51 +0800

    config mysql

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'itcast',
+        'PASSWORD': '123456',
+        'NAME': 'meiduo',
     }
 }
 
