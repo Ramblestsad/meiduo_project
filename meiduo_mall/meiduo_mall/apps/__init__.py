7e119aafcc6a58835ea538a474f3e1bce50b4ea1
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2020-12-23 16:31:40 +0800

    install app users

---
+++ meiduo_mall/meiduo_mall/apps/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+
+'''
+Author: Chris W.
+Date: 2020-12-23 16:13:00
+LastEditTime: 2020-12-23 16:13:00
+LastEditors: Chris W.
+Description: None
+Version: 1.0
+License: MIT License
+Copyright 2020 Chris W.
+'''
+
+
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'meiduo_mall.apps.users'  # app [users]
 ]
 
 MIDDLEWARE = [
