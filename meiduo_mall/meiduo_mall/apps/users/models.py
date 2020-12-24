f3eef12bfe5f2d383a5ab3f04f6dc56b851682df
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2020-12-24 14:48:05 +0800

    create users models class and migrate

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """description: custom user model class"""
+
+    mobile = models.CharField(max_length=11, unique=True, verbose_name='cellphone number')
+
+    class meta:
+
+        db_table = 'tb_users'  # custom table name
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
