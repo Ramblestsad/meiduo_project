4e2c09a7f970e202a7c2984c7f1591cdcab01ca1
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2021-01-30 15:01:46 +0800

    goods - added FastDFS env

---
+++ meiduo_mall/meiduo_mall/utils/fastdfs/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+
+'''
+Author: Chris W.
+Date: 2021-01-30 14:50:56
+LastEditTime: 2021-01-30 14:50:56
+LastEditors: Chris W.
+Description: None
+Version: 1.0
+License: MIT License
+Copyright 2021 Chris W.
+'''
+
+
