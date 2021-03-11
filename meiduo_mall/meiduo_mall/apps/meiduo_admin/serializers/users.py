dcf929090a3a932cd44e8a051ad1d43b24554280
Ramblestsad <45003009+Ramblestsad@users.noreply.github.com>
2021-03-11 23:38:32 +0800

    admin - users--add new users from admin

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
@@ -21,4 +21,33 @@ class UsersSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'mobile', 'email')
+        fields = ('id', 'username', 'mobile', 'email', 'password')
+        # 为字段添加额外验证内容,
+        # 主键id默认有read_only: True选项，不参与反序列化
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'max_length': 20,
+                'min_length': 8
+            },
+            'username': {
+                'max_length': 20,
+                'min_length': 5
+            }
+        }
+
+    def create(self, validated_data):
+        """
+            重写保存create方法，加密password
+            !!!IMPORTANT: DRF 增、改都在serializer中完成
+        """
+
+        # user = super().create(validated_data=validated_data)
+        # # Django框架自带用户模型类方法set_password对密码进行加密
+        # user.set_password(validated_data['password'])
+        # user.save()
+
+        # Method 2:
+        user = User.objects.create_user(**validated_data)
+
+        return user
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
@@ -11,7 +11,7 @@ License: None
 """
 
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
 
 from users.models import User
@@ -19,7 +19,7 @@ from meiduo_admin.serializers.users import UsersSerializer
 from meiduo_admin.utils import PageNum
 
 
-class UsersView(ListAPIView):
+class UsersView(ListCreateAPIView):
     """获取用户数据"""
 
     permission_classes = [IsAdminUser]
@@ -27,7 +27,7 @@ class UsersView(ListAPIView):
     # 指定查询集，重写 get_queryset 方法后就不会调用下面的queryset了
     # queryset = User.objects.all().order_by('id')
 
-    # 指定序列化器
+    # 指定序列化器: 多个业务调用一个serializer可能会漏掉某些字段
     serializer_class = UsersSerializer
 
     # 使用分页器
@@ -37,7 +37,7 @@ class UsersView(ListAPIView):
     def get_queryset(self):
 
         if self.request.query_params.get('keyword') == '':
-            return User.objects.all().order_by('-id')
+            return User.objects.all().order_by('id')
         else:
             username = self.request.query_params.get('keyword')
             # username__contains 模糊查询
